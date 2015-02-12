fe6e6d25c3f0cfcf97e34c3dafa0c13b0eac3ee2
hbhk <1024784402@qq.com>
2015-02-12 13:43:25 +0800

    提交国际化修改

--- aili/aili-core/src/main/java/org/hbhk/aili/core/server/deploy/spring/ModuleInterceptor.java
+++ aili/aili-core/src/main/java/org/hbhk/aili/core/server/deploy/spring/ModuleInterceptor.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.hbhk.aili.core.server.context.RequestContext;
 import org.hbhk.aili.core.share.util.MyStringUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -21,10 +22,16 @@ public class ModuleInterceptor extends HandlerInterceptorAdapter {
 			return;
 		}
 		HandlerMethod m = (HandlerMethod) handler;
-		//获取模块名
-		String  clsName =m.getBean().getClass().getName();
-		String[] arr = clsName.split("\\.");
-		String moduleName = arr[3];
+		RequestMapping rm = m.getMethodAnnotation(RequestMapping.class);
+		String moduleName = null;
+		if (rm != null && rm.value().length > 0) {
+			moduleName = rm.value()[0];
+		} else {
+			String clsName = m.getBean().getClass().getName();
+			String[] arr = clsName.split("\\.");
+			moduleName = arr[3];
+		}
+		// 获取模块名
 		String contextPath = request.getContextPath() + "/";
 		request.setAttribute("base", contextPath);
 		if (modelAndView != null
@@ -56,16 +63,16 @@ public class ModuleInterceptor extends HandlerInterceptorAdapter {
 	}
 
 	private boolean filter(String viewName) {
-		if (viewName.startsWith("redirect:")){
+		if (viewName.startsWith("redirect:")) {
 			return false;
 		}
-		if (viewName.startsWith("http:")){
+		if (viewName.startsWith("http:")) {
 			return false;
 		}
-		if (viewName.startsWith("https:")){
+		if (viewName.startsWith("https:")) {
 			return false;
 		}
-		if(MyStringUtils.mulStrCount(viewName, "/")>1){
+		if (MyStringUtils.mulStrCount(viewName, "/") > 1) {
 			return false;
 		}
 
