f1ea39211f5f937d5dc4ea3faa7242733de21347
hbhk <1024784402@qq.com>
2015-02-13 14:41:19 +0800

    提交国际化修改

--- aili/aili-mybatis/src/main/java/org/hbhk/aili/mybatis/server/support/AutoCreateTable.java
+++ aili/aili-mybatis/src/main/java/org/hbhk/aili/mybatis/server/support/AutoCreateTable.java
@@ -24,7 +24,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 
 public class AutoCreateTable implements InitializingBean {
 
